b6ab9e93f7a2f23f84c365c0e40363b1066639b9
Saverio Scumaci <svscumaci@knox.edu>
2023-01-18 13:10:40 -0600

    Start of main menu

---
+++ Assets/titleScreenScripts/mainMenuScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame(){
+
+    }
+
+    public void openCredits(){
+
+    }
+
+    public void openHighScores(){
+
+    }
+
+    public void toggleAudio(){
+        
+    }
+}
