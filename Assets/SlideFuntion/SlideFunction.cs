76f8ec1f5d6d58057ea2d0b46ce588041c3a8e02
CVenegas2022 <cavenegas@knox.edu>
2023-01-23 12:45:15 -0600

    Update SlideFunction.cs

--- Assets/SlideFuntion/SlideFunction.cs
+++ Assets/SlideFuntion/SlideFunction.cs
@@ -4,58 +4,17 @@ using UnityEngine;
 
 public class SlideFunction : MonoBehaviour
 {
-    public BoxCollider2D targetCollider;
-    public BoxCollider2D ground;
-    public Transform targetTransform;
-    public float newHeight;
-    private float baseHeight;
-    private Vector2 baseSize;
-    private Vector2 newSize;
-    private Vector3 currLocation;
-    private Vector3 newLocation;
-    private bool isChanged;
+    
     // Start is called before the first frame update
     void Start()
     {
-        //baseHeight=targetCollider.size.y;
-        baseSize=targetCollider.size;
-        newSize=new Vector2(targetCollider.size.x, newHeight);
-        isChanged=false;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if(targetCollider.IsTouching(ground) && Input.GetKey("down"))
-        {
-            if(!isChanged)
-            {
-                currLocation=targetTransform.position;
-                newLocation=currLocation;
-            
-                if(newHeight>baseHeight)
-                {
-                    newLocation.y+=(newHeight/2);
-                }
-                else if(newHeight<baseHeight)
-                {
-                    newLocation.y-=newHeight;
-                }
-                targetTransform.position=newLocation;
-                targetCollider.size=newSize;
-                isChanged=true;
-                //play animation
-            }
-        }
-        else
-        {
-            if(isChanged)
-            {
-                transform.position=currLocation;
-                targetCollider.size=baseSize;
-                //stop animation
-            }
-        }
+        
     }
 }
