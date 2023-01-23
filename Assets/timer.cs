4ffed2fd69d933c233a1599d455299c8ae11d6f0
Saverio Scumaci <svscumaci@knox.edu>
2023-01-23 12:27:24 -0600

    removed meta in gitIgnore and re-added timer for score

---
+++ Assets/timer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    [Header("Component")]
+    [Header("Timer Settings")]
+    public float currentTime;
+    public bool countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+        timerText.text = currentTime.ToString();
+    }
+}
