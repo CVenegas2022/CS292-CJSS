2ed265d241f3b28b1fa8be94d27fb56bc37ca8b3
CVenegas2022 <cavenegas@knox.edu>
2023-01-25 02:07:06 -0600

    Fixed animation issues and cleaned up code in Jump.  Also updated scene and Player(1) prefabs accordingly

--- Assets/JumpFunctionAssets/Scripts/Jump.cs
+++ Assets/JumpFunctionAssets/Scripts/Jump.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    public float jumpSpeed = 30f;
+    public float jumpSpeed = 40f;
     public float maxHeight = 5f;
     public float jumpGrav = 0f;
-    public float fallGrav = 7f;
+    public float fallGrav = 5f;
     public BoxCollider2D ground;
     private bool isGrounded;
     private bool canTravel;
-    private bool needsAnimationSwap;
     private bool keyHeld;
     private Vector3 jumpPos;
-    //public Vector2 force=new Vector2(0,10,0);
 
-    // Only works if the player can actually touch the max height.  Gravity scale 0 for jumpGrav gives desired effect. 
+    // Used these two sources to get animation to work correctly https://www.youtube.com/watch?v=07GUMwiOPBE and 
+    // https://guilbomadev.medium.com/unity-trigger-an-animation-through-code-5242aef3a578
+
+    // Only works if the player can actually touch the max height
+     
     // Start is called before the first frame update
     void Start()
     {
+        //initialize isGrounded to false
         isGrounded=false;
+
+        //initialize canTravel to false
         canTravel=false;
-        needsAnimationSwap=true;
+
+        //initialize keyHeld to true
         keyHeld=true;
     }
 
@@ -35,67 +41,90 @@ public class Jump : MonoBehaviour
     void LateUpdate()
     {
         //used this forum to figure out how to reference an object's BoxCollider2D: https://forum.unity.com/threads/check-if-two-colliders-are-touching.693817/
+        
+        //if touching the ground
         if(gameObject.GetComponent<BoxCollider2D>().IsTouching(ground))
         {
+            //set jumpPos to current position with max height added to the y value
             jumpPos = new Vector3(transform.position.x, transform.position.y+maxHeight, transform.position.z);
+            
+            //if not marked as on the ground
+            if(!isGrounded)
+            {
+                //play the run animation
+                GetComponent<Animator>().SetTrigger("endJump");
+            }
+            //mark as on the ground
             isGrounded=true;
+
+            //if key is marked as held and up is not being pressed
             if(keyHeld && !Input.GetKey("up"))
             {
+                //mark as key not being held
                 keyHeld=false;
             }
+            
+            //allow player to jump
             canTravel=true;
+
+            //set gravity to desired jump gravity
             GetComponent<Rigidbody2D>().gravityScale=jumpGrav;
-            if(needsAnimationSwap)
-            {
-                GetComponent<Animator>().Play("Base Layer.Run");
-                needsAnimationSwap=false;
-            }
         }
+        //if not on the ground
         else
         {
+            //mark as not on the ground
             isGrounded=false;
+
+            //if at or over desired max height
             if(transform.position.y>=jumpPos.y)
             {
+                //prevent travel
                 canTravel=false;
             }
         }
 
+        //if the player is able to move upwards
         if(canTravel)
         {
-            //pressing up button
+            //if pressing up button
             if(Input.GetKey("up"))
             {
+                //if on the ground and the key isn't held from last jump
                 if(isGrounded && !keyHeld)
                 {
+                    //mark key as being held down
                     keyHeld=true;
-                    GetComponent<Animator>().Play("Base Layer.Jump");
-                    needsAnimationSwap=true;
+                    
+                    //play jump animation
+                    GetComponent<Animator>().SetTrigger("startJump");
+
+                    //move towards jumpPos
                     transform.position = Vector3.MoveTowards(transform.position, jumpPos, jumpSpeed * Time.deltaTime);
                 }
+                //if the player is not on the ground
                 else if(!isGrounded)
                 {
+                    //move towards jumpPos
                     transform.position = Vector3.MoveTowards(transform.position, jumpPos, jumpSpeed * Time.deltaTime);
                 }
-                //GetComponent<Rigidbody2D>().AddForce(transform.up * jumpSpeed, ForceMode2D.Impulse);
             }
-            //release up button
+            //if not pressing up button
             else
             {
+                //mark key as not being held
                 keyHeld=false;
+                //prevent the player from being able to move up
                 canTravel=false;
             }
 
         }
+        //if the player can no longer move up
         else
         {
             //set to higher gravity
             GetComponent<Rigidbody2D>().gravityScale=fallGrav;
         }
         
-    }
-
-    public bool getIsGrounded()
-    {
-        return isGrounded;
     }
 }
