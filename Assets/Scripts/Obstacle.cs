daafe0d2d9e943188ac049fe67434490eec4157d
jessie <huquach@knox.edu>
2023-02-04 22:52:57 -0600

    improved the functionality of Obstacles and created demo2

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     public float speed;
+    // private float time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,19 @@ public class Obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // time += Time.deltaTime;
+        // if (time < 20) {
+        //     // speed = 6;
+        //     transform.position += Vector3.left * 6 * Time.deltaTime;
+        // }
+        // else if (time >= 25 && time < 45) {
+        //     // speed = 7;
+        //     transform.position += Vector3.left * 7 * Time.deltaTime;
+        // }
+        // else if (time >= 50) {
+        //     // speed = 8;
+        //     transform.position += Vector3.left * 8 * Time.deltaTime;
+        // }
         transform.position += Vector3.left * speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject easyObstacle1;
-    public GameObject easyObstacle2;
-    public GameObject easyObstacle3;
-    public GameObject mediumObstacle1;
-    public GameObject mediumObstacle2;
-    public GameObject hardObstacle1;
-    public GameObject hardObstacle2;
-    public GameObject hardObstacle3;
-    public GameObject hardObstacle4;
+    public GameObject easyObstacle1_1;
+    public GameObject easyObstacle1_2;
+    public GameObject easyObstacle1_3;
+    public GameObject easyObstacle2_1;
+    public GameObject easyObstacle2_2;
+    public GameObject easyObstacle2_3;
+    public GameObject mediumObstacle2_1;
+    public GameObject mediumObstacle2_2;
+    public GameObject easyObstacle3_1;
+    public GameObject easyObstacle3_2;
+    public GameObject easyObstacle3_3;
+    public GameObject mediumObstacle3_1;
+    public GameObject mediumObstacle3_2;
+    public GameObject hardObstacle3_1;
+    public GameObject hardObstacle3_2;
+    public GameObject hardObstacle3_3;
+    public GameObject hardObstacle3_4;
     public float timeToSpawn = 1.8f;
     private float timer = 0;
     private float time = 0;
@@ -28,117 +36,120 @@ public class Spawner : MonoBehaviour
         time += Time.deltaTime;
         if (time < 20) {
             if (timer > timeToSpawn) {
-                if (timer < 1.8015) {
+                float randomNumber = Random.Range(0f, 100f);
+                if (randomNumber < 33) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle1 = Instantiate(easyObstacle1);
+                    GameObject newEasyObstacle1 = Instantiate(easyObstacle1_1);
                     Destroy(newEasyObstacle1, 10);
                     timer = 0;
                 } 
-                else if (timer >= 1.8015 && timer < 1.803) {
+                else if (randomNumber >= 33 && randomNumber < 66) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle2 = Instantiate(easyObstacle2);
+                    GameObject newEasyObstacle2 = Instantiate(easyObstacle1_2);
                     Destroy(newEasyObstacle2, 10);
                     timer = 0;
                 }
                 else {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle3 = Instantiate(easyObstacle3);
+                    GameObject newEasyObstacle3 = Instantiate(easyObstacle1_3);
                     Destroy(newEasyObstacle3, 10);
                     timer = 0;
                 }
             }
         }
 
-        if (time >= 20 && time < 40) {
+        if (time >= 25 && time < 45) {
             timeToSpawn = 1.5f;
             if (timer > timeToSpawn) {
-                if (timer < 1.501) {
+                float randomNumber = Random.Range(0f, 100f);
+                if (randomNumber < 20) {
                     // Debug.Log(timer);
-                    GameObject newMediumObstacle1 = Instantiate(mediumObstacle1);
+                    GameObject newMediumObstacle1 = Instantiate(mediumObstacle2_1);
                     Destroy(newMediumObstacle1, 10);
                     timer = 0;
                 } 
-                else if (timer >= 1.501 && timer < 1.502) {
+                else if (randomNumber >= 20 && randomNumber < 40) {
                     // Debug.Log(timer);
-                    GameObject newMediumObstacle2 = Instantiate(mediumObstacle2);
+                    GameObject newMediumObstacle2 = Instantiate(mediumObstacle2_2);
                     Destroy(newMediumObstacle2, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.502 && timer < 1.503) {
+                else if (randomNumber >= 40 && randomNumber < 60) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle3 = Instantiate(easyObstacle3);
+                    GameObject newEasyObstacle3 = Instantiate(easyObstacle2_3);
                     Destroy(newEasyObstacle3, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.503 && timer < 1.507) {
+                else if (randomNumber >= 60 && randomNumber < 80) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle1 = Instantiate(easyObstacle1);
+                    GameObject newEasyObstacle1 = Instantiate(easyObstacle2_1);
                     Destroy(newEasyObstacle1, 10);
                     timer = 0;
                 }
                 else {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle2 = Instantiate(easyObstacle2);
+                    GameObject newEasyObstacle2 = Instantiate(easyObstacle2_2);
                     Destroy(newEasyObstacle2, 10);
                     timer = 0;
                 }
             }
         }
 
-        if (time >= 40) {
+        if (time >= 50) {
             timeToSpawn = 1.2f;
             if (timer > timeToSpawn) {
-                if (timer < 1.2005) {
+                float randomNumber = Random.Range(0f, 200f);
+                if (randomNumber < 20) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle1 = Instantiate(easyObstacle1);
+                    GameObject newEasyObstacle1 = Instantiate(easyObstacle3_1);
                     Destroy(newEasyObstacle1, 10);
                     timer = 0;
                 } 
-                else if (timer >= 1.2005 && timer < 1.201) {
+                else if (randomNumber >= 20 && randomNumber < 40) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle2 = Instantiate(easyObstacle2);
+                    GameObject newEasyObstacle2 = Instantiate(easyObstacle3_2);
                     Destroy(newEasyObstacle2, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.201 && timer < 1.2015) {
+                else if (randomNumber >= 40 && randomNumber < 60) {
                     // Debug.Log(timer);
-                    GameObject newEasyObstacle3 = Instantiate(easyObstacle3);
+                    GameObject newEasyObstacle3 = Instantiate(easyObstacle3_3);
                     Destroy(newEasyObstacle3, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.2015 && timer < 1.202) {
+                else if (randomNumber >= 60 && randomNumber < 80) {
                     // Debug.Log(timer);
-                    GameObject newMediumObstacle1 = Instantiate(mediumObstacle1);
+                    GameObject newMediumObstacle1 = Instantiate(mediumObstacle3_1);
                     Destroy(newMediumObstacle1, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.2025 && timer < 1.203) {
+                else if (randomNumber >= 80 && randomNumber < 100) {
                     // Debug.Log(timer);
-                    GameObject newMediumObstacle2 = Instantiate(mediumObstacle2);
+                    GameObject newMediumObstacle2 = Instantiate(mediumObstacle3_2);
                     Destroy(newMediumObstacle2, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.203 && timer < 1.205) {
+                else if (randomNumber >= 100 && randomNumber < 125) {
                     // Debug.Log(timer);
-                    GameObject newHardObstacle1 = Instantiate(hardObstacle1);
+                    GameObject newHardObstacle1 = Instantiate(hardObstacle3_1);
                     Destroy(newHardObstacle1, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.205 && timer < 1.207) {
+                else if (randomNumber >= 125 && randomNumber < 150) {
                     // Debug.Log(timer);
-                    GameObject newHardObstacle2 = Instantiate(hardObstacle2);
+                    GameObject newHardObstacle2 = Instantiate(hardObstacle3_2);
                     Destroy(newHardObstacle2, 10);
                     timer = 0;
                 }
-                else if (timer >= 1.207 && timer < 1.209) {
+                else if (randomNumber >= 150 && randomNumber < 175) {
                     // Debug.Log(timer);
-                    GameObject newHardObstacle2 = Instantiate(hardObstacle2);
+                    GameObject newHardObstacle2 = Instantiate(hardObstacle3_3);
                     Destroy(newHardObstacle2, 10);
                     timer = 0;
                 }
                 else {
                     // Debug.Log(timer);
-                    GameObject newHardObstacle3 = Instantiate(hardObstacle3);
+                    GameObject newHardObstacle3 = Instantiate(hardObstacle3_4);
                     Destroy(newHardObstacle3, 10);
                     timer = 0;
                 }
