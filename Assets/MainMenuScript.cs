6857264b8f31423814a16c0bb7ed50aadf8db773
Saverio Scumaci <svscumaci@knox.edu>
2023-02-13 13:20:31 -0600

    Fixed hardFire Hit box and made the pause menu with resume, restart, and quit buttons.

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -6,33 +6,35 @@ using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
     public string gameScene;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        // Ensure that the gameIsPaused flag is set to false when the scene starts
+        pauseMenuScript.gameIsPaused = false;
+        Time.timeScale = 1f;
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene(gameScene);
+        SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+        pauseMenuScript.gameIsPaused = false;
     }
 
     public void toggleAudio()
     {
-
+        // Add audio toggle logic here
     }
+
     public void openCredits()
     {
-
+        // Add logic to open the credits here
     }
+
     public void openHighScores()
     {
-
+        // Add logic to open the high scores here
     }
+    
 }
+
+
