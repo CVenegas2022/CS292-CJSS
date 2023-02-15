47632e2a737a5b6ab74c9064bf6cd737d666f688
jessie <huquach@knox.edu>
2023-02-15 12:55:13 -0600

    implemented second gameover condition

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 //using System.Collections;
 
 public class PlayerCollision : MonoBehaviour
@@ -15,6 +16,7 @@ public class PlayerCollision : MonoBehaviour
             if(hbHealthManager.totalCherries <= 0)
             {
                 //gameover
+                SceneManager.LoadScene("gameOverScreenDemo2-1");
             }
         }
     }
