0c84ca684cd5a01ebce63040a1fdf06cf708c249
CVenegas2022 <cavenegas@knox.edu>
2023-02-15 13:50:23 -0600

    added damage immunity and visual cues.  Also, edited the width of hard spikes obstacle

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -3,16 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 //using System.Collections;
-
 public class PlayerCollision : MonoBehaviour
 {
+    public int seconds = 3;
     public int c = 0;
+    public SpriteRenderer playerSprite;
+    private float iFrames;
+    private float frameTracker;
+    private bool isInvincible;
+    private Color baseColor;
+    private Color phantom;
+
+    void Start()
+    {
+        iFrames=60*seconds;
+        frameTracker=0f;
+        isInvincible=false;
+        baseColor=playerSprite.color;
+        phantom=baseColor;
+        phantom.a=0.7f;
+    }
+
+    void Update()
+    {
+        if(isInvincible)
+        {
+            if(frameTracker<=iFrames)
+            {
+                frameTracker++;
+            }
+            else
+            {
+                isInvincible=false;
+                frameTracker=0f;
+                playerSprite.color=baseColor;
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == "mediumSpike(Clone)" || collision.gameObject.name == "hardFires(Clone)" || collision.gameObject.name == "hardSpear(Clone)" || collision.gameObject.name == "hardSpike(Clone)")
+        if((collision.gameObject.name == "mediumSpike(Clone)" || collision.gameObject.name == "hardFires(Clone)" || collision.gameObject.name == "hardSpear(Clone)" || collision.gameObject.name == "hardSpike(Clone)") && !isInvincible)
         {
 
             hbHealthManager.totalCherries --;
+            isInvincible=true;
+            playerSprite.color=phantom;
+
             if(hbHealthManager.totalCherries <= 0)
             {
                 //gameover
